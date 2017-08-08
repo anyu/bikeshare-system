ec925227c9a0f7f0efc3ce92d4a342ef02e84e66
An Yu <ayu3500@gmail.com>
2017-08-07 17:26:33 -0700

    (feat) Add Member post/put/delete resources

--- server/controllers/members.js
+++ server/controllers/members.js
@@ -1,12 +1,77 @@
 const models = require('../../db/models');
 const knex = require('../../db/config').knex;
 
-module.exports.getAll = (req, res) => {
+module.exports.getAllMembers = (req, res) => {
   models.Member.fetchAll()
     .then(members => {
-      res.status(200).send(members);
+      res.status(200).json({members});
     })
     .catch(err => {
       res.status(503).send(err);
+    });
+};
+
+module.exports.addMember = (req, res) => {
+  models.Member.forge({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email
+  }).save()
+    .then((saved) => {
+      res.status(200).json({saved});
+    })
+    .error(err => {
+      res.status(500).send(err);
+    })
+    .catch(() => {
+      res.sendStatus(404);
+    });
+};
+
+module.exports.getMember = (req, res) => {
+  models.Member.where({ id: req.params.id }).fetch()
+    .then(member => {
+      if (!member) {
+        throw member;
+      }
+      res.status(200).json({member});
+    })
+    .error(err => {
+      res.status(500).send(err);
+    })
+    .catch(() => {
+      res.sendStatus(404);
+    });
+};
+
+module.exports.updateMember = (req, res) => {
+  models.Member.where({ id: req.params.id }).fetch()
+    .then(member => {
+      if (!member) {
+        throw member;
+      }
+      return member.save(req.body, { method: 'update' });
+    })
+    .then(() => {
+      res.sendStatus(201);
+    })
+    .error(err => {
+      res.status(500).send(err);
+    })
+    .catch(() => {
+      res.sendStatus(404);
+    });
+};
+
+module.exports.deleteMember = (req, res) => {
+  models.Member.where({ id: req.params.id }).destroy()
+    .then((destroyed) => {
+      res.status(200).json({destroyed});
+    })
+    .error(err => {
+      res.status(500).send(err);
+    })
+    .catch(() => {
+      res.sendStatus(404);
     });
 };
--- server/routes/api.js
+++ server/routes/api.js
@@ -14,7 +14,15 @@ router.route('/')
     res.status(201).send({ data: 'Posted' });
   });
 
-router.route('/members').get(MemberController.getAll);
+router.route('/members')
+  .get(MemberController.getAllMembers)
+  .post(MemberController.addMember)
+;
 
+router.route('/members/:id')
+  .get(MemberController.getMember)
+  .put(MemberController.updateMember)
+  .delete(MemberController.deleteMember)
+;
 
 module.exports = router;
