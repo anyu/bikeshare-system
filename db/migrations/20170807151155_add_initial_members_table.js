290d13ab8c0766e5b7ac979477fc455578b726f4
An Yu <ayu3500@gmail.com>
2017-08-07 18:18:56 -0700

    (feat) Modify members schema, add bikes/stations

--- db/migrations/20170807151155_add_initial_members_table.js
+++ db/migrations/20170807151155_add_initial_members_table.js
@@ -1,15 +1,37 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema
-      .createTableIfNotExists('members', function(table) {
-        table.increments('id').primary();
-        table.string('firstName');
-        table.string('lastName');
-        table.string('email');
-      });  
+  return Promise.all([
+    knex.schema.createTableIfNotExists('members', (table) => {
+      table.increments('id').unsigned().primary();
+      table.string('first_name');
+      table.string('last_name');
+      table.string('email').unique();
+      table.integer('ride_count').unsigned();
+      table.boolean('is_active');
+      table.string('has_rent_access');
+    }),
+    knex.schema.createTableIfNotExists('stations', (table) => {
+      table.increments('id').unsigned().primary();
+      table.integer('bike_count').unsigned();      
+      table.boolean('is_empty');
+    }),
+    knex.schema.createTableIfNotExists('bikes', (table) => {
+      table.increments('id').unsigned().primary();
+      table.boolean('is_available');
+      table.integer('docked_station_id').references('stations.id').onDelete('CASCADE');
+      table.integer('active_rider').references('members.id').onDelete('CASCADE');
+      table.integer('last_rider').references('members.id').onDelete('CASCADE');
+    })
+  ]);
 };
 
 exports.down = function(knex, Promise) {
   return knex.schema
-    .dropTableIfExists('members');  
+    .dropTableIfExists('bikes')
+    .then(() => {
+      return knex.schema.dropTableIfExists('stations');
+    })
+    .then(() => {
+      return knex.schema.dropTableIfExists('members');
+    })    
 };
