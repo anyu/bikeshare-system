0760fffd96c69c178740fe33c3cfa5e87ee391af
An Yu <ayu3500@gmail.com>
2017-08-07 19:29:46 -0700

    (feat) Use more scalable table columns; add bikes_stations table

--- db/migrations/20170807151155_add_initial_members_table.js
+++ db/migrations/20170807151155_add_initial_members_table.js
@@ -6,14 +6,15 @@ exports.up = function(knex, Promise) {
       table.string('first_name');
       table.string('last_name');
       table.string('email').unique();
+      table.string('status');
+      table.string('access_level');      
       table.integer('ride_count').unsigned();
-      table.boolean('is_active');
-      table.string('has_rent_access');
     }),
     knex.schema.createTableIfNotExists('stations', (table) => {
       table.increments('id').unsigned().primary();
       table.integer('bike_count').unsigned();      
-      table.boolean('is_empty');
+      table.integer('max_capacity').unsigned();
+      table.integer('percent_full').unsigned();
     }),
     knex.schema.createTableIfNotExists('bikes', (table) => {
       table.increments('id').unsigned().primary();
@@ -21,17 +22,24 @@ exports.up = function(knex, Promise) {
       table.integer('docked_station_id').references('stations.id').onDelete('CASCADE');
       table.integer('active_rider').references('members.id').onDelete('CASCADE');
       table.integer('last_rider').references('members.id').onDelete('CASCADE');
-    })
+    }),
+    knex.schema.createTableIfNotExists('bikes_stations', (table) => {
+      table.integer('bike_id').references('bikes.id').onDelete('CASCADE');
+      table.integer('station_id').references('stations.id');
+    }),    
   ]);
 };
 
 exports.down = function(knex, Promise) {
   return knex.schema
-    .dropTableIfExists('bikes')
+    .dropTableIfExists('bikes_stations')
     .then(() => {
-      return knex.schema.dropTableIfExists('stations');
+      return knex.schema.dropTableIfExists('bikes');
     })
     .then(() => {
-      return knex.schema.dropTableIfExists('members');
+      return knex.schema.dropTableIfExists('stations');
     })    
+    .then(() => {
+      return knex.schema.dropTableIfExists('members');
+    })       
 };
