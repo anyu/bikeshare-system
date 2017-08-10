8b38e2ddc8e779b9aae72f409bb746a6a9e6d900
An Yu <ayu3500@gmail.com>
2017-08-09 23:16:57 -0700

    (feat) Fix bike model relationships

--- db/models/bike.js
+++ db/models/bike.js
@@ -6,14 +6,14 @@ bookshelf.plugin('pagination');
 
 const Bike = bookshelf.Model.extend({
   tableName: 'bikes',
-  docked_station: function() {
-    return this.belongsTo('Station');
+  docked_station_id: function() {
+    return this.belongsTo('Station', 'docked_station_id');
   },   
-  active_rider: function() {
-    return this.belongsTo('Member');
+  active_rider_id: function() {
+    return this.belongsTo('Member', 'active_rider_id');
   },  
-  last_rider: function() {
-    return this.belongsTo('Member');
+  last_rider_id: function() {
+    return this.belongsTo('Member', 'last_rider_id');
   },   
 });
 
