6d4d84742eaaad653e6b68aab570e943d8fe7d4e
An Yu <ayu3500@gmail.com>
2017-08-09 23:26:55 -0700

    (feat) Chain seed data creation to prevent foreign key violation error

--- db/seeds/bikes.js
+++ db/seeds/bikes.js
@@ -1,31 +1,31 @@
-let generateBikes = (knex, index, is_available, docked_station_id, active_rider_id, last_rider_id) => {
-  return knex('bikes').insert([
-    {id: index, is_available: is_available, docked_station_id: docked_station_id, 
-    active_rider_id: active_rider_id, last_rider_id: last_rider_id}
-  ])
-};
+// let generateBikes = (knex, index, is_available, docked_station_id, active_rider_id, last_rider_id) => {
+//   return knex('bikes').insert([
+//     {id: index, is_available: is_available, docked_station_id: docked_station_id, 
+//     active_rider_id: active_rider_id, last_rider_id: last_rider_id}
+//   ])
+// };
 
-getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //Max is exclusive, min is inclusive
-}
+// getRandomInt = (min, max) => {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min)) + min; //Max is exclusive, min is inclusive
+// }
 
 exports.seed = function(knex, Promise) {
   // Deletes all existing entries
-  return knex('bikes').del()
-   .then(() => {
-    // Inserts seed entries
-    let records = [];
-    let availability = [true, false];
+  // return knex('bikes').del()
+  //  .then(() => {
+  //   // Inserts seed entries
+  //   let records = [];
+  //   let availability = [true, false];
 
-    for (let i = 1; i <= 200; i++) {
-      let randomAvailability = availability[getRandomInt(0, availability.length)];
-      let randomDockedStation = getRandomInt(0, 10);
-      let randomActiveRider = getRandomInt(0, 1000);
-      let randomLastRider = getRandomInt(0, 1000);
-      records.push(generateBikes(knex, i, randomAvailability, randomDockedStation, randomActiveRider, randomLastRider));
-    }
-    return Promise.all(records);
-  });
+  //   for (let i = 1; i <= 200; i++) {
+  //     let randomAvailability = availability[getRandomInt(0, availability.length)];
+  //     let randomDockedStation = getRandomInt(1, 11);
+  //     let randomActiveRider = getRandomInt(1, 1001);
+  //     let randomLastRider = getRandomInt(1, 1001);
+  //     records.push(generateBikes(knex, i, randomAvailability, randomDockedStation, randomActiveRider, randomLastRider));
+  //   }
+  //   return Promise.all(records);
+  // });
 };
--- db/seeds/stations.js
+++ db/seeds/stations.js
@@ -7,18 +7,18 @@ let generateStations = (knex, index, bike_count, max_capacity, percent_full) => 
 
 exports.seed = function(knex, Promise) {
   // Deletes all existing entries
-  return knex('stations').del()
-    .then(() => {
-      // Inserts seed entries
-      let records = [];
-      let maxCapacityOptions = [10, 20, 30, 40, 50];
+  // return knex('stations').del()
+  //   .then(() => {
+  //     // Inserts seed entries
+  //     let records = [];
+  //     let maxCapacityOptions = [10, 20, 30, 40, 50];
 
-      for (let i = 1; i <= 10; i++) {
-        let randomMaxCapacity = maxCapacityOptions[getRandomInt(0, maxCapacityOptions.length)];
-        let randomBikeCount = getRandomInt(0, randomMaxCapacity);
-        let percentFull = Math.floor((randomBikeCount/randomMaxCapacity)*100);
-        records.push(generateStations(knex, i, randomBikeCount, randomMaxCapacity, percentFull));
-      }
-      return Promise.all(records);
-    });
+  //     for (let i = 1; i <= 10; i++) {
+  //       let randomMaxCapacity = maxCapacityOptions[getRandomInt(0, maxCapacityOptions.length)];
+  //       let randomBikeCount = getRandomInt(0, randomMaxCapacity);
+  //       let percentFull = Math.floor((randomBikeCount/randomMaxCapacity)*100);
+  //       records.push(generateStations(knex, i, randomBikeCount, randomMaxCapacity, percentFull));
+  //     }
+  //     return Promise.all(records);
+  //   });
 };
