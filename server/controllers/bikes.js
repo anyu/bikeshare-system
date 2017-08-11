dfd14e24a0565906fa0dc44b2ca44b4bf8e98705
An Yu <ayu3500@gmail.com>
2017-08-10 17:42:45 -0700

    (feat) Update station and member info on bike return

--- server/controllers/bikes.js
+++ server/controllers/bikes.js
@@ -75,7 +75,10 @@ module.exports.rentBike = (req, res) => {
         throw bike;
       }
       if (bike.attributes.is_available) {
+        // Q: Should route be bikes/id/stations/station_id/rent instead?
+        // Q: Should there be a check to see if this bike is at this station, or unnecessary. 
         //TODO: mark not at station instead of station 1
+        //TODO: update member status to active; decrement station's bike_count, recalculate percent full
         var params = {'docked_station_id': 1, 'active_rider_id': req.params.member_id, 'is_available': false};
         return bike.save(params, {method: 'update',patch: true})
         .then(() => {
@@ -93,33 +96,47 @@ module.exports.rentBike = (req, res) => {
     });
 };
 
-// When renting
-// Check if bike 4 is available.
-//    -- if not, return err saying bike can't be rented.
-// If so, update dockedstation to 0, active rider to member id, is available to false.
-
-// When returning
-// Set active rider to 0, last_rider to member id returning the bike, set is_available to true
-
 module.exports.returnBike = (req, res) => {
   models.Bike.where({ id: req.params.id }).fetch()
     .then((bike) => {
       if (!bike) {
         throw bike;
       }
-      //TODO: mark not at station instead of station 1
-      var params = {'docked_station_id': req.body.station_id, 'active_rider_id': 1, 'last_rider_id': req.params.member_id, 'is_available': true};
-      return bike.save(params, {method: 'update',patch: true})
-      .then(() => {
-        res.status(200).json({bike});
+      models.Station.where({id: req.body.station_id}).fetch()
+      .tap((station) => {
+        if (station.attributes.percent_full < 100) {
+          var params = {'docked_station_id': station.attributes.id, 'active_rider_id': 1, 'last_rider_id': req.params.member_id, 'is_available': true};
+          return bike.save(params, {method: 'update',patch: true})
+          .then(() => {
+            var newPercentFull = Math.floor(((station.attributes.bike_count+1)/station.attributes.max_capacity)*100);
+            var params = {'bike_count': station.attributes.bike_count+1, 'percent_full': newPercentFull};
+            return station.save(params, {method: 'update',patch: true})
+            .then(() => {
+              models.Member.where({id: req.params.member_id}).fetch()
+              .then((member) => {
+                var params = {'ride_count': member.attributes.ride_count+1, 'status': 'inactive'};
+                return member.save(params, {method: 'update',patch: true})
+                .then(() => {
+                  res.status(200).json({bike, station, member});
+                })
+              })
+              .catch((err) => {
+                res.sendStatus(404);
+              });
+            });
+          })
+          .catch((err) => {
+            res.sendStatus(404);
+          });
+        } else {
+          res.send("This station is full. Please return your bike at another station.");
+        }
       })
       .catch((err) => {
         res.sendStatus(404);
-      });
+      });        
     })
-    .catch((err) => {
-      res.sendStatus(404);
-    });
+      //TODO: increment member's ride_count, update status to active; increment station's bike count
 };
 /***************************************** TODO ends *************************************************/
 
