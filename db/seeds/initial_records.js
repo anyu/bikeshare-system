8677d469896eda7f03fce876c8f295323a3c9e94
An Yu <ayu3500@gmail.com>
2017-08-16 13:36:21 -0700

    (refactor) Generate seed data for trips table

--- db/seeds/initial_records.js
+++ db/seeds/initial_records.js
@@ -20,9 +20,9 @@ let generateBikes = (knex, index, status, docked_station_id) => {
 };
 
 
-let generateTrips = (knex, index, status, rider_id, bike_id, start_station_id, end_station_id) => {
+let generateTrips = (knex, index, status, start_time, end_time, rider_id, bike_id, start_station_id, end_station_id) => {
   return knex('trips').insert([
-    {id: index, status: status, start_time: start_time, end_time: end_time, start_station_id: start_station_id, end_station_id: end_station_id}
+    {id: index, status: status, start_time: start_time, end_time: end_time, rider_id: rider_id, bike_id: bike_id, start_station_id: start_station_id, end_station_id: end_station_id}
   ])
 };
 
@@ -31,6 +31,21 @@ getRandomInt = (min, max) => {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //Max is exclusive, min is inclusive
 }
+
+
+getRandomPastTimes = () => {
+  let times = [];
+  let startDateTime = new Date();
+  let endDateTime = new Date();
+  let randomDaysAgo = getRandomInt(10, 100);
+  let randomDuration = getRandomInt(1, 10);
+
+  startDateTime.setDate(startDateTime.getDate() - randomDaysAgo);
+  endDateTime.setHours(startDateTime.getHours() + randomDuration);
+  times.push(startDateTime, endDateTime);
+
+  return(times);
+ }
 
 
 exports.seed = function(knex, Promise) {
@@ -73,20 +88,37 @@ exports.seed = function(knex, Promise) {
             bikeRecords.push(generateBikes(knex, i, randomStatus, randomDockedStation));
           }
           return Promise.all(bikeRecords);
-        });
-        // TO DO: seed trips (need to generate timestamps, start/end stations that make sense)
-        // .then(() => {
-        //   let tripRecords = [];
-        //   let status = [null, 'ongoing', 'ended'];
+        })
+        .then(() => {
+          let tripRecords = [];
+         
+          // seed ended trips
+          for (let i = 1; i <= 250; i++) {
+            let status = 'ended';
+            let randomPastTimes = getRandomPastTimes();
+            let randomStartTime = randomPastTimes[0];
+            let randomEndTime = randomPastTimes[1];
+            let randomRider = getRandomInt(1, 1000);
+            let randomBike = getRandomInt(1, 200);            
+            let randomStartStation = getRandomInt(1, 11);
+            let randomEndStation = getRandomInt(1, 11);
+            tripRecords.push(generateTrips(knex, i, status, randomStartTime, randomEndTime, randomRider, randomBike, randomStartStation, randomEndStation));
+          }
 
-        //   for (let i = 1; i <= 200; i++) {
-        //     let randomStatus = status[getRandomInt(0, status.length)];
-        //     let randomStartStation = getRandomInt(1, 11);
-        //     let randomEndStation = getRandomInt(1, 11);
-        //     tripRecords.push(generateTrips(knex, i, status, rider_id, bike_id, start_station_id, end_station_id));
-        //   }
-        //   return Promise.all(tripRecords);
-        // });        
+          // seed ongoing trips
+          for (let i = 251; i <= 500; i++) {
+            let status = 'ongoing';
+            let randomPastTimes = getRandomPastTimes();
+            let randomStartTime = randomPastTimes[0];
+            let randomEndTime = null;
+            let randomRider = getRandomInt(1, 1000);
+            let randomBike = getRandomInt(1, 200);            
+            let randomStartStation = getRandomInt(1, 11);
+            let randomEndStation = null;
+            tripRecords.push(generateTrips(knex, i, status, randomStartTime, randomEndTime, randomRider, randomBike, randomStartStation, randomEndStation));
+          }          
+          return Promise.all(tripRecords);
+        });        
       });
     });
   });
